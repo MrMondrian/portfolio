2af0cdcecefb509e80530dd38fd62df090c2fb1c
MrMondrian <anthonytan012012@gmail.com>
2022-07-13 16:38:06 -0500

    email js is working, made tweeks to buttons

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -8,12 +8,18 @@ const Contact = () => {
 
     const sendEmail = (e) => {
         e.preventDefault();
-
-        emailjs.sendForm('YOUR_SERVICE_ID', 'YOUR_TEMPLATE_ID', form.current, 'tOMRgyl-KZk7jjlrW')
+        
+        emailjs.sendForm('service_e22v4fq', 'template_satg68h', form.current, 'tOMRgyl-KZk7jjlrW')
             .then((result) => {
+                const tags = document.getElementsByClassName('form_input')
+                tags[0].value = '';
+                tags[1].value = '';
+                tags[2].value = '';
                 console.log(result.text);
+                alert("Message sent!")
             }, (error) => {
                 console.log(error.text);
+                alert("Message failed to send.\nYou can email me directly at anthonytan8@protonmail.com")
             });
     };
   return (
@@ -26,7 +32,7 @@ const Contact = () => {
             <input className='form_input' type='text' name='name' placeholder='Your Name' required></input>
             <input className='form_input' type='text' name='email' placeholder='Your Email' required></input>
             <textarea className='form_input' type='text' name='message' rows='7' placeholder='Type your message here' required></textarea>
-            <button type='submit' class='btn contact_btn'>Send message</button>
+            <button type='submit' className='btn contact_btn'>Send message</button>
         </form>
     </section>
   )
