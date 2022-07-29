035949d9692ac7744e5a8168a78577e844cd3497
MrMondrian <anthonytan012012@gmail.com>
2022-07-28 20:37:17 -0500

    fixed buttons

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 import './project.css'
 
 
-const project = ({title, info, picture}) => {
+const project = ({title, info, picture,buttons}) => {
+
+  const the_buttons = []
+  buttons.forEach((bt) => {
+    the_buttons.push(<a className='btn'>{bt.text}</a>)
+  })
   return (
     <article>
         <div className='project_container'>
@@ -12,7 +17,7 @@ const project = ({title, info, picture}) => {
                 <div className='project_info'>
                     <p className='project_info_text'>{info}</p>
                     <div className='project_buttons'>
-                        <button className='btn'>Try it yourself</button>
+                        {the_buttons}
                     </div>
                 </div>
                 
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -8,7 +8,8 @@ const data = {
   title: 'GeSort',
   picture: GeSortPicture,
   info: 'GeSort is a citizen science game made to help solve the genome sorting problem. It was developed at the University of Manitoba\
-  Bioinformatics lab. I worked on this project under the supervision of Dr. Tremblay-Savard during my interships at his lab summer 2020 and 2021.'
+  Bioinformatics lab. I worked on this project under the supervision of Dr. Tremblay-Savard during my interships at his lab summer 2020 and 2021.',
+  buttons: [{text: 'Try it yourself', link:'https://gesort.cs.umanitoba.ca:9001/'}]
 }
 
 const projects = () => {
