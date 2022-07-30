bc36d43dc19eb2dc1978e047e845ae6fec9a21cf
MrMondrian <anthonytan012012@gmail.com>
2022-07-29 22:55:36 -0500

    video for projects is working

--- src/components/project/project.jsx
+++ src/components/project/project.jsx
@@ -27,4 +27,31 @@ const project = ({title, info, picture,buttons}) => {
   )
 }
 
+const ProjectVideo = ({title, info, video,buttons}) => {
+    const the_buttons = []
+    buttons.forEach((bt) => {
+        the_buttons.push(<a className='btn'>{bt.text}</a>)
+    })
+    return (
+        <article>
+            <div className='project_container'>
+                <h3>{title}</h3>
+                <div className='project_content'>
+                    <video width="370" height="370"className='project_vid' controls loop autoplay>
+                        <source src={video} type="video/mp4"/>
+                    </video>
+                    <div className='project_info'>
+                        <p className='project_info_text'>{info}</p>
+                        <div className='project_buttons'>
+                            {the_buttons}
+                        </div>
+                    </div>
+                    
+                </div>
+            </div>
+        </article>
+    )
+}
+
 export default project
+export {ProjectVideo}
--- src/components/projects/projects.jsx
+++ src/components/projects/projects.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './projects.css'
-import Project from '../project/project'
+import Project, {ProjectVideo} from '../project/project'
+import '../project/project.css'
 
 import GeSortPicture from '../../assets/GeSort2.png'
+import ArcadePicture from '../../assets/Arcade.png'
+import ArcadeVideo from '../../assets/ArcadeVideo.mp4'
 
-const data = {
+const project1 = {
   title: 'GeSort',
   picture: GeSortPicture,
   info: 'GeSort is a citizen science game made to help solve the genome sorting problem. It was developed at the University of Manitoba\
@@ -12,13 +15,22 @@ const data = {
   buttons: [{text: 'Try it yourself', link:'https://gesort.cs.umanitoba.ca:9001/'}]
 }
 
+const project2 = {
+  title: 'Arcade Shooter Game',
+  video: ArcadeVideo,
+  info: 'It is an arcade shooter built in java that features a mix of 2d and 3d graphics',
+  buttons: []
+}
+
+const tags = [<Project {...project1}/>, <ProjectVideo {...project2}/>]
+
 const projects = () => {
   return (
     <section id='projects'>
         <div className='projects_container'>
             <h2>Projects</h2>
             <div className="projects_list">
-              <Project {...data}/>
+              {tags} 
             </div>
             
         </div>
